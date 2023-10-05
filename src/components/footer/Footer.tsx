cc8d5462e25b667c35d62bc6d1439aa86bfc1fd4
Filipe Quintanilha <feliipequintanilha@gmail.com>
2023-10-05 01:09:45 -0300

    feat(footer): finaliza footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../sass/footer.scss";
+
+function Footer() {
+
+  return (
+    <footer>  
+      <p>M3: Implantação de E-commerce VTEX</p>
+    </footer>
+  );
+}
+
+export default Footer;
